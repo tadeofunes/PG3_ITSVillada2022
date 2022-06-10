6749789d809c044e51a0c5bd08b5c9386c6e5ea1
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2022-06-10 08:47:41 -0300

    Update Ej6.py

--- Exepcionespy/Ej6.py
+++ Exepcionespy/Ej6.py
@@ -1,18 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
 
-cnx = mysql.connector.connect(user='bdi', password='pepe1234', host='127.0.0.1', database='empresa')
+cnx = mysql.connector.connect(user='bdi', password='pepe1234', host='127.0.0.1', database='Cantantes')
 cursor = cnx.cursor()
 try:
-    add_medico = ("INSERT INTO MEDICOS "
-                 "(dni, nombre)"
-                 "VALUES (%s, %s)")
+    add_cantante = ("INSERT INTO cantante " "(dni, nombre)""VALUES (%s, %s)")
 
-    data_medico = (33457892, 'Ricardo Moroni')
+    data_cantante = (33457892, 'Ricardo Moroni')
 
-    cursor.execute(add_medico, data_medico)
+    cursor.execute(add_cantante, data_cantante)
     cnx.commit()
+    
 except mysql.connector.errors.IntegrityError:
-    print("hubo un problema al insertar el medico")
+    print("Hay un problema en insertar un cantante")
 cursor.close()
 cnx.close()
